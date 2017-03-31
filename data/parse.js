1d5493e367639513177f6794091b695a7826716f
Cyril Diagne <cyril.diagne@ecal.ch>
2017-03-31 12:36:01 +0200

    add parser to get students.json from ecal intranet page

---
+++ data/parse.js
@@ -0,0 +1,23 @@
+let fs = require('fs');
+let cheerio = require('cheerio');
+
+let data = fs.readFileSync(__dirname + '/students.html');
+let $ = cheerio.load(data, {decodeEntities : false});
+
+let students = $('li.boma .ds-line');
+let result = [];
+
+students.each((i, el) => {
+  let name = $(el).find('li:nth-child(1)').html();
+  let role = $(el).find('li:nth-child(2)').html();
+  let desc = $(el).find('li:nth-child(3)').html();
+  let img = $(el).find('.photo > div').attr('data-img');
+  let email = $(el).find('.card > a').text();
+  result.push(
+      {name : name, role : role, desc : desc, img : img, email : email});
+});
+
+// remove first empty line
+result.shift();
+
+fs.writeFileSync('students.json', JSON.stringify(result, null, 2));
