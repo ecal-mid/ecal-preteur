9464ac434a18df594ead7dff1634ac5015741eab
Cyril Diagne <cyril.diagne@ecal.ch>
2017-04-09 16:46:14 +0200

    remove unused data from json

--- data/parse.js
+++ data/parse.js
@@ -2,19 +2,22 @@ let fs = require('fs');
 let cheerio = require('cheerio');
 
 let data = fs.readFileSync(__dirname + '/students.html');
-let $ = cheerio.load(data, {decodeEntities : false});
+let $ = cheerio.load(data, {decodeEntities: false});
 
 let students = $('li.boma .ds-line');
 let result = [];
 
 students.each((i, el) => {
   let name = $(el).find('li:nth-child(1)').html();
-  let role = $(el).find('li:nth-child(2)').html();
-  let desc = $(el).find('li:nth-child(3)').html();
+  // let role = $(el).find('li:nth-child(2)').html();
+  // let desc = $(el).find('li:nth-child(3)').html();
   let img = $(el).find('.photo > div').attr('data-img');
+  if (img) {
+    img = img.split('/');
+    img = img[img.length - 1];
+  }
   let email = $(el).find('.card > a').text();
-  result.push(
-      {name : name, role : role, desc : desc, img : img, email : email});
+  result.push({name: name, img: img, email: email});
 });
 
 // remove first empty line
